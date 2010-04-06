eb0045db15db73742c252109d0802bb6a32482ff
HCK <HCK@8a9b1199-d942-744a-a8b1-ac58f4d8385e>
2010-04-06 15:18:04 +0000

    Copyrigth auf GPL umgestellt.
    git-svn-id: https://inovat-svn-server:8443/svn/NI-DAUZ-1/trunk@201 8a9b1199-d942-744a-a8b1-ac58f4d8385e

--- src/de/bsvrz/kex/tls/osi2osi3/osi2/tlsoip/DefaultProperties.java
+++ src/de/bsvrz/kex/tls/osi2osi3/osi2/tlsoip/DefaultProperties.java
@@ -1,22 +1,25 @@
 /*
- * Copyright (c) 2009 by inovat, innovative systeme - verkehr - tunnel - technik, Dipl.-Ing. H. C. Kniß
- * ALL RIGHTS RESERVED.
+ * Copyright (c) 2010 by inovat, innovative systeme - verkehr - tunnel - technik, Dipl.-Ing. H. C. Kniss
  *
- * THIS SOFTWARE IS  PROVIDED  "AS IS"  AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF  MERCHANTABILITY  AND  FITNESS  FOR  A PARTICULAR  PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL inovat OR ITS CONTRIBUTORS BE
- * LIABLE FOR ANY  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
- * OR CONSEQUENTIAL DAMAGES  (INCL., BUT NOT LIMITED TO, PROCUREMENT
- * OF SUBSTITUTE GOODS OR SERVICES;  LOSS OF USE, DATA, OR  PROFITS;
- * OR BUSINESS INTERRUPTION)  HOWEVER  CAUSED  AND  ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,  OR TORT (INCL.
- * NEGLIGENCE OR OTHERWISE)  ARISING  IN  ANY  WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * This file is part of de.bsvrz.kex.tls.osi2osi3.osi2.tlsoip.DefaultProperties
+ *
+ * de.bsvrz.kex.tls.osi2osi3.osi2.tlsoip.DefaultProperties is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * de.bsvrz.kex.tls.osi2osi3.osi2.tlsoip.DefaultProperties is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with de.bsvrz.kex.tls.osi2osi3.osi2.tlsoip.DefaultProperties; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  * Contact Information:
- * inovat, Dipl.-Ing. H. C. Kniß
- * Kölner Straße 30
+ * inovat, Dipl.-Ing. H. C. Kniss
+ * Koelner Strasse 30
  * D-50859 Köln
  * +49 (0)2234 4301 800
  * info@invat.de
